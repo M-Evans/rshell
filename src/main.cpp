529cef9b71e3290d9d5fb2dc9fa2e549f5ed70e4
M-Evans <mevansprogramming@gmail.com>
2015-01-17 22:28:37 -0800

    added libraries, some variables, an infinite loop, and an input scheme.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <vector>
+#include <string>
+#include <iostream>
 
-int main() {
-    printf("Hello world!\n");
+int main(int argc, char** argv) {
+    std::string line;                // hold a raw line of input
+    std::vector<char*> cmd;          // holds a single command and its arguments
+    std::vector<std::vector<char*> > cmds; // holds multiple commands
+
+    printf("\nWelcome to rshell!\n");
+
+    while (1) {
+        printf("$ ");
+        getline(std::cin, line);
+        if (line.substr(0, 4) == "exit") {
+            printf("Goodbye!\n");
+            exit(0);
+        }
+        printf("You entered \"%s\"\n", line.c_str());
+    }
     return 0;
 }
