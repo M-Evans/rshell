9f0dd591d45570fefcabfbc9346c7cdfe18021c1
M-Evans <mevansprogramming@gmail.com>
2015-01-17 23:06:30 -0800

    Added support for comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,49 @@
 #include <string>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    std::string line;                // hold a raw line of input
-    std::vector<char*> cmd;          // holds a single command and its arguments
-    std::vector<std::vector<char*> > cmds; // holds multiple commands
+int main(int argc, char** argv)
+{
+    // hold a raw line of input
+    std::string line;
 
-    printf("\nWelcome to rshell!\n");
+    // print welcome message and prompt
+    printf("Welcome to rshell!\n$ ");
+    getline(std::cin, line);
 
-    while (1) {
-        printf("$ ");
-        getline(std::cin, line);
-        if (line.substr(0, 4) == "exit") {
+    do
+    {
+        // holds a single command and its arguments
+        std::vector<char*> cmd;
+        // holds multiple commands
+        std::vector<std::vector<char*> > cmds;
+
+        // look for comments
+        if (line.find("#") != std::string::npos) {
+            // remove them if necessary
+            line = line.substr(0, line.find("#"));
+        }
+
+        // add a space so that every space-separated entity has
+        // at least one space directly to the right of it
+        // (makes parsing easier)
+        line += ' ';
+
+        // if exit was entered properly
+        if (line.substr(0,5) == "exit ")
+        {
+            // say goodbye, and quit
             printf("Goodbye!\n");
             exit(0);
         }
+
+        // handle the input
         printf("You entered \"%s\"\n", line.c_str());
-    }
+
+        // todo: split 
+        
+        
+        // print prompt and get a line of text
+        printf("$ ");
+    } while (getline(std::cin, line));
     return 0;
 }
