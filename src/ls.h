f506bfbddaaa184f2485c968e16f983a9db26e2c
Michael Evans <mevansprogramming@gmail.com>
2015-02-08 07:29:16 -0800

    Added the ability to print file info (-l), and stubbed a few functions to make it better

--- src/ls.h
+++ src/ls.h
@@ -1,4 +1,15 @@
 #include <string.h>
+#include <libgen.h> // dirname and basename
+#include <unistd.h>
+#include <math.h>
+#include <pwd.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <vector>
+#include <iostream>
 
 bool compareFilenamesInefficient(const char* a, const char* b)
 {
@@ -18,11 +29,108 @@ bool compareFilenamesInefficient(const char* a, const char* b)
   return ret;
 }
 
+void printFilePrepl() {} // TODO: calculate and pass back
+// the widths and coloration info
+
+void printFilePrep() {} // TODO: calculate and pass back
+// the widths and coloration info. This version only gets
+// the spacing for the non -l output. (no stat widths)
 
 
 
 
+void printFiles(const std::vector<char*> v, bool l) {
+  for(unsigned i = 0; i < v.size(); ++i) {
+    if (l) {
+      // get the file stats
+      struct stat fs;
+      if (stat(v[i], &fs) == -1) {
+        perror("stat");
+        exit(1);
+      }
 
+      // file type
+      if (S_ISREG(fs.st_mode)) { printf("-"); }
+      else if (S_ISDIR(fs.st_mode)) { printf("d"); }
+      else if (S_ISCHR(fs.st_mode)) { printf("c"); }
+      else if (S_ISBLK(fs.st_mode)) { printf("b"); }
+      else if (S_ISFIFO(fs.st_mode)) { printf("p"); }
+      else if (S_ISLNK(fs.st_mode)) { printf("l"); }
+      else if (S_ISSOCK(fs.st_mode)) { printf("s"); }
+      else { printf("?"); }
+
+      // permissions
+      printf("%c%c%c%c%c%c%c%c%c",
+              (fs.st_mode & S_IRUSR) ? 'r' : '-',
+              (fs.st_mode & S_IWUSR) ? 'w' : '-',
+              (fs.st_mode & S_IXUSR) ? 'x' : '-',
+              (fs.st_mode & S_IRGRP) ? 'r' : '-',
+              (fs.st_mode & S_IWGRP) ? 'w' : '-',
+              (fs.st_mode & S_IXGRP) ? 'x' : '-',
+              (fs.st_mode & S_IROTH) ? 'r' : '-',
+              (fs.st_mode & S_IWOTH) ? 'w' : '-',
+              (fs.st_mode & S_IXOTH) ? 'x' : '-');
+
+      // inode count (number of hard links)
+      printf(" %2u", (unsigned)fs.st_nlink);
+
+      // owner
+      struct passwd* name;
+      if ((name = getpwuid(fs.st_uid)) == NULL)
+      {
+        // no passwd struct returned
+        // if errno is set, perror it
+        if (errno) {
+          perror("getpwuid");
+          exit(1);
+        }
+        // else, nothing was returned but there was no error
+        // give it something to print
+        name->pw_name = (char*)"noName";
+      }
+      printf(" %8s", name->pw_name);
+
+      // group
+      if ((name = getpwuid(fs.st_gid)) == NULL)
+      {
+        // no passwd struct returned
+        // if errno is set, perror it
+        if (errno) {
+          perror("getpwuid");
+          exit(1);
+        }
+        // else, nothing was returned but there was no error
+        // give it something to print
+        name->pw_name = (char*)"noName";
+      }
+      printf(" %8s", name->pw_name);
+
+      // size
+      printf(" %6u", (unsigned)fs.st_size);
+      
+      // date
+      // broken down time info gets held in the 
+      // tm struct
+      struct tm fileMtime;
+      // use fs.st_mtime to create fileMtime
+      localtime_r(&fs.st_mtime, &fileMtime);
+      // 20 chars for the resulting string. Should NEVER be more
+      char timestr[20];
+      // get formatted time string
+      // %b: short month    %d: 00->31 day
+      // %H: 00->23 hour    %M: 00->59 minute
+      // fileMtime is accessed, timestr gets modified
+      strftime((char*)&timestr, 20, " %b %d %H:%M", &fileMtime);
+      // actually print the time
+      printf("%s", timestr);
+      // print the filename (just the name)
+      // TODO: color the files
+      printf(" %s\n", basename(v[i]));
+    } else {
+      printf("%s  ", basename(v[i]));
+    }
+  }
+}
 
 
 
