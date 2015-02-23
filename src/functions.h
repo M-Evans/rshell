2363a4b178871f0cc8fbde204ea38afc639eafd1
Michael Evans <mevansprogramming@gmail.com>
2015-02-22 16:29:41 -0800

    changed style

--- src/functions.h
+++ src/functions.h
@@ -1,13 +1,14 @@
-#include <cstdlib>
 #include <string>
 #include <vector>
 #include <cctype>
+#include <stdlib.h>
 
 
 
 #define GETWORD    0
 #define TRIMSPACE  1
 #define HANDLESEMI 2
+#define REDIR      3
 
 #define NONE 0
 #define AND  1
@@ -17,77 +18,77 @@
 #define debug(A) std::cout << #A << ": " << A << std::endl
 
 // struct for holding a command and what its connector is
-struct Command
-{
-    std::vector<char*> args;
-    int connector;
+struct Command {
+  std::vector<char*> args;
+  int connector;
+
+  // constructor
+  Command() {
+    connector = 0;
+  }
 };
 
 
 // copies a string into a cstring, while being careful of
 // memory management. Do NOT write past the end of the cstring
-char* stocstr(const std::string& s)
-{
-    // allocate memory for the cstring (+1 for the NULL char)
-    char* c = new char[s.size() + 1];
+char* stocstr(const std::string& s) {
+  // allocate memory for the cstring (+1 for the NULL char)
+  char* c = new char[s.size() + 1];
 
-    // copy the string over
-    for(unsigned i = 0; i < s.size(); ++i)
-    {
-        c[i] = s[i];
-    }
+  // copy the string over
+  for(unsigned i = 0; i < s.size(); ++i) {
+    c[i] = s[i];
+  }
 
-    // finish off with NULL char
-    c[s.size()] = '\0';
+  // finish off with NULL char
+  c[s.size()] = '\0';
 
-    return c;
+  return c;
 }
 
 
-bool isConn(char c)
-{
-    switch(c)
-    {
-        case ';':
-        case '&':
-        case '|':
-        case '<':
-        case '>':
-            return true;
-            break;
-        default:
-            return false;
-    }
+bool isRedir(char c) {
+  switch(c) {
+    case '>':
+    case '<':
+      return true;
+      break;
+    default:
+      return false;
+  }
+}
+
+bool isConn(char c) {
+  switch(c) {
+    case ';':
+    case '&':
+    case '|':
+      return true;
+      break;
+    default:
+      return false;
+  }
 }
 
 
 
 void handleCon(std::vector<Command>& cmds, Command& cmd, const std::string& line,
-       int& mode, unsigned& begin, unsigned& i, bool& se)
-{
-  switch(line[i])
-  {
+               int& mode, unsigned& begin, unsigned& i, bool& se) {
+  switch(line[i]) {
     case '&':
       // make sure we don't access past what we're supposed to
-      if (i + 1 < line.size() && line[i+1] == '&')
-      {
+      if (i + 1 < line.size() && line[i+1] == '&') {
         i++;
         // check if there's more to parse after this command
-        if (i + 1 < line.size())
-        {
+        if (i + 1 < line.size()) {
           // there is
-          if (line[i + 1] == '&' || line[i + 1] == '|')
-          {
+          if (line[i + 1] == '&' || line[i + 1] == '|') {
             // unfortunately, the next thing is a connector. This is a syntax error.
             se = true;
             break;
-          }
-          else if (isspace(line[i + 1])) // it's a space. go into TRIMSPACE mode
-          {
+          } else if (isspace(line[i + 1])) { // it's a space. go into TRIMSPACE mode
             mode = TRIMSPACE;
-          }
-          else // it's neither a connector nor a space: must be a word
-          {
+          } else { // it's neither a connector nor a space: must be a word
             mode = GETWORD;
             begin = i + 1;
           }
@@ -97,33 +98,24 @@ void handleCon(std::vector<Command>& cmds, Command& cmd, const std::string& line
         // prep cmd for next use
         cmd.args.clear(); // remove command and arguments for the next bit of parsing
         cmd.connector = NONE;
-      }
-      else // if the next thing isn't another &, syntax error
-      {
+      } else { // if the next thing isn't another &, syntax error
         se = true;
       }
       break;
     case '|':
       // make sure we don't access past what we're supposed to
-      if (i + 1 < line.size() && line[i+1] == '|')
-      {
+      if (i + 1 < line.size() && line[i+1] == '|') {
         i++;
         // check if there's more to parse after this command
-        if (i + 1 < line.size())
-        {
+        if (i + 1 < line.size()) {
           // there is
-          if (line[i + 1] == '&' || line[i + 1] == '|')
-          {
+          if (line[i + 1] == '&' || line[i + 1] == '|') {
             // unfortunately, the next thing is a connector. This is a syntax error.
             se = true;
             break;
-          }
-          else if (isspace(line[i + 1])) // it's a space. go into TRIMSPACE mode
-          {
+          } else if (isspace(line[i + 1])) { // it's a space. go into TRIMSPACE mode
             mode = TRIMSPACE;
-          }
-          else // it's neither a connector nor a space: must be a word
-          {
+          } else { // it's neither a connector nor a space: must be a word
             mode = GETWORD;
             begin = i + 1;
           }
@@ -133,36 +125,26 @@ void handleCon(std::vector<Command>& cmds, Command& cmd, const std::string& line
         // prep cmd for next use
         cmd.args.clear(); // remove command and arguments for the next bit of parsing
         cmd.connector = NONE;
-      }
-      else // if the next thing isn't another |, syntax error (for now. :P)
-      {
+      } else { // if the next thing isn't another |, syntax error (for now. :P)
         se = true;
       }
       break;
     case ';':
       // there is always more to parse
-      if (line[i + 1] == '&' || line[i + 1] == '|')
-      {
+      if (line[i + 1] == '&' || line[i + 1] == '|') {
         // unfortunately, the next thing is a continuation connector. This is a syntax error.
         se = true;
         break;
-      }
-      else if (isspace(line[i + 1])) // it's a space. go into TRIMSPACE mode
-      {
+      } else if (isspace(line[i + 1])) { // it's a space. go into TRIMSPACE mode
         mode = TRIMSPACE;
-      }
-      else if (line[i + 1] == ';')
-      {
+      } else if (line[i + 1] == ';') {
         mode = HANDLESEMI;
-      }
-      else // it's neither a connector nor a space: must be a word
-      {
+      } else { // it's neither a connector nor a space: must be a word
         mode = GETWORD;
         begin = i + 1;
       }
 
-      if (!cmd.args.empty()) // if there are commands in the command vector
-      {
+      if (!cmd.args.empty()) { // if there are commands in the command vector
         cmd.connector = SEMI; // the current command has an semicolon connector
         cmds.push_back(cmd); // add the command to the vector of commands that need to be executed
         // prep cmd for next use
@@ -177,4 +159,25 @@ void handleCon(std::vector<Command>& cmds, Command& cmd, const std::string& line
 }
 
 
+void preProcessLine(std::string& line) {
+  getline(std::cin, line);
 
+  if (std::cin.fail()) {
+    printf("\nGoodbye!\n");
+    exit(0);
+  }
+
+  // look for comments
+  if (line.find("#") != std::string::npos) {
+    // remove them if necessary (they're useless)
+    line = line.substr(0, line.find("#"));
+  }
+
+  // remove leading whitespace
+  while (line.size() > 0 && (line[0] == ' ' || line[0] == '\t' || line[0] == ';')) {
+    line = line.substr(1, line.size() - 1);
+  }
+
+  // adding this makes parsing easier
+  line += "; ";
+}
--- src/main.cpp
+++ src/main.cpp
@@ -12,43 +12,34 @@
 #include "functions.h"
 
 
-int main(int argc, char** argv)
-{
-  // print welcome message and prompt
-  printf("Welcome to rshell!\n");
-
+int main(int argc, char** argv) {
   std::string prompt;
 
   char* cbuff = getlogin();
 
-  if (cbuff == NULL)
-  {
-    perror("getlogin");
-  }
-  else
-  {
+  if (cbuff == NULL) {
+    perror("getlogin failed");
+  } else {
     prompt += cbuff;
-  }
 
-  cbuff = new char[4096];
-  if (gethostname(cbuff, (unsigned)4096) == -1)
-  {
-    perror("gethostname");
-  }
-  else
-  {
-    prompt += '@';
-    prompt += cbuff;
-  }
+    cbuff = new char[4096];
+    if (gethostname(cbuff, (unsigned)4096) == -1) {
+      perror("gethostname failed");
+    } else {
+      prompt += '@';
+      prompt += cbuff;
+    }
 
-  delete[] cbuff;
+    delete[] cbuff;
+  }
 
   prompt += " > ";
-
   bool ext = false;
 
-  while (!ext)
-  {
+  // print welcome message and prompt
+  printf("Welcome to rshell!\n");
+
+  while (!ext) {
     // holds a single command and its arguments
     Command cmd;
     // holds multiple commands
@@ -58,29 +49,7 @@ int main(int argc, char** argv)
 
     // print prompt and get a line of text (done in condition)
     printf("%s", prompt.c_str());
-    getline(std::cin, line);
-
-    // look for comments
-    if (line.find("#") != std::string::npos)
-    {
-      // remove them if necessary (they're useless)
-      line = line.substr(0, line.find("#"));
-    }
-
-    // remove leading whitespace
-    while (line.size() > 0 && (line[0] == ' ' || line[0] == '\t' || line[0] == ';'))
-    {
-      line = line.substr(1, line.size() - 1);
-    }
-
-    // adding this makes parsing easier
-    line += "; ";
-
-    if (std::cin.fail())
-    {
-      printf("\nGoodbye!\n");
-      exit(0);
-    }
+    preProcessLine(line);
 
     int mode = GETWORD;
     unsigned begin = 0;
@@ -92,78 +61,47 @@ int main(int argc, char** argv)
     bool se = false;
 
     // starts with a connector? I don't think so
-    if (line.size() > 0 && isConn(line[0]) && line[0] != ';')
-    {
+    if (line.size() > 0 && isConn(line[0]) && line[0] != ';') {
       se = true;
     }
 
     // handle the input
-    for(unsigned i = 0; i < line.size() && !se; ++i)
-    {
+    for(unsigned i = 0; i < line.size() && !se; ++i) {
       bool con   = isConn(line[i]);
       bool space = isspace(line[i]);
 
       // if we're getting a word and there's a whitespace or connector here
-      if (mode == GETWORD && (space || con))
-      {
+      if (mode == GETWORD && (space || con)) {
         // chunk the last term and throw it into the vector
         char* c = stocstr(line.substr(begin, i - begin));
-        if (strlen(c) > 0)
-        {
+        if (strlen(c) > 0) {
           cmd.args.push_back(c);
-        }
-        else
-        {
+        } else {
           // only happens when nothing is entered. breaks so nothing more happens
           break;
         }
 
-        if (space)
-        {
+        if (space) {
           mode = TRIMSPACE;
-        }
-        else // it's a connector
-        {
+        } else { // it's a connector
           handleCon(cmds, cmd, line, mode, begin, i, se);
         }
-      }
-      else if (mode == TRIMSPACE && (!space || con))
-      {
-        if (con && cmd.args.empty())
-        {
-
-// (cmds[cmds.size() - 1].connector == AND || cmds[cmds.size() - 1].connector == OR))
-
-          // if (line[i] != ';' || cmds[cmds.size() - 1].connector !=
-          // {
-            // why this test? two reasons:
-            //   1) it 
+      } else if (mode == TRIMSPACE && !space) {
+        if (con && cmd.args.empty()) {
           if (line[i] != ';')
             se = true;
-          // }
-        }
-        else if (con)
-        {
+        } else if (con) {
           handleCon(cmds, cmd, line, mode, begin, i, se);
-        }
-        else
-        {
+        } else {
           mode = GETWORD;
           begin = i;
         }
-      }
-      else if (mode == HANDLESEMI && line[i] != ';')
-      {
-        if (isConn(line[i]))
-        {
+      } else if (mode == HANDLESEMI && line[i] != ';') {
+        if (isConn(line[i])) {
           se = true;
-        }
-        else if (isspace(line[i]))
-        {
+        } else if (isspace(line[i])) {
           mode = TRIMSPACE;
-        }
-        else // it's a word
-        {
+        } else { // it's a word
           mode = GETWORD;
           begin = i;
         }
@@ -172,82 +110,62 @@ int main(int argc, char** argv)
 
     // if the last command has a continuation connector, syntax error
     if (cmds.size() > 0 && (cmds[cmds.size() - 1].connector == AND
-                        ||  cmds[cmds.size() - 1].connector == OR))
-    {
+                        ||  cmds[cmds.size() - 1].connector == OR)) {
       se = true;
     }
 
     // if there was a syntax error
-    if (se)
-    {
+    if (se) {
       printf("Syntax error\n");
       continue;
     }
 
     // now to execute all the commands
-    for(unsigned i = 0; i < cmds.size(); ++i)
-    {
+    for(unsigned i = 0; i < cmds.size(); ++i) {
       int exitStatus = 0;
       char* arg = cmds[i].args[0];
-      if (strcmp(arg, "exit") == 0)
-      {
+      if (strcmp(arg, "exit") == 0) {
         ext = true;
         break;
       }
       char** argv = new char*[cmds[i].args.size() + 1];
-      for(unsigned j = 0; j < cmds[i].args.size(); ++j)
-      {
+      for(unsigned j = 0; j < cmds[i].args.size(); ++j) {
         argv[j] = cmds[i].args[j];
       }
       argv[cmds[i].args.size()] = 0;
 
       // arg and argv are now prepared
       pid_t pid = fork();
-      if (pid == -1)
-      {
+      if (pid == -1) {
         perror("fork");
         exit(1);
-      }
-      else if (pid == 0) // child process
-      {
-        if (execvp(arg, argv) == -1)
-        {
+      } else if (pid == 0) { // child process
+        if (execvp(arg, argv) == -1) {
           // if there's a return value, there was a problem
           // -1 indicates a problem, specifically
-          perror("execvp");
+          perror(arg);
           delete[] argv;
           exit(1);
         }
-      }
-      else // parent process
-      {
-        if (waitpid(pid, &exitStatus, 0) == -1)
-        {
+      } else { // parent process
+        if (waitpid(pid, &exitStatus, 0) == -1) {
           perror("waitpid");
           exit(1);
         }
-      }
-      if (!exitStatus) // all is good (0)
-      {
-        while (i < cmds.size() && cmds[i].connector == OR)
-        {
+      } if (!exitStatus) { // all is good (0)
+        while (i < cmds.size() && cmds[i].connector == OR) {
           ++i;
         }
-      }
-      else // last command failed
-      {
-        while (i < cmds.size() && cmds[i].connector == AND)
-        {
+      } else { // last command failed
+        while (i < cmds.size() && cmds[i].connector == AND) {
           ++i;
         }
       }
     }
 
     // deallocate allocated memory
-    for(unsigned i = 0; i < cmds.size(); ++i)
-    {
-      for(unsigned j = 0; j < cmds[i].args.size(); ++j)
-      {
+    for(unsigned i = 0; i < cmds.size(); ++i) {
+      for(unsigned j = 0; j < cmds[i].args.size(); ++j) {
         delete[] cmds[i].args[j];
       }
     }
